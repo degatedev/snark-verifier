de1a7ff54db1b4bbef67010d0ee355d457325088
xrqin <qxrmath@gmail.com>
2024-03-16 11:00:06 +0800

    constraint three commitment

--- snark-verifier-sdk/src/halo2/aggregation.rs
+++ snark-verifier-sdk/src/halo2/aggregation.rs
@@ -239,7 +239,23 @@ where
     
         // Assuming proof_transcripts[0][0] and proof_transcripts[0][1] are the TranscriptObjects you want to compare
         if let (TranscriptObject::EcPoint(lhs_point), TranscriptObject::EcPoint(rhs_point)) = 
-            (&proof_transcripts[0][0], &proof_transcripts[1][0]) 
+            (&proof_transcripts[0][0], &proof_transcripts[3][0]) 
+        {
+            loader.ec_point_assert_eq("constraint ec point", lhs_point, rhs_point);
+        } else {
+            panic!("Expected EC points in the transcript objects");
+        }
+
+        if let (TranscriptObject::EcPoint(lhs_point), TranscriptObject::EcPoint(rhs_point)) = 
+            (&proof_transcripts[1][0], &proof_transcripts[3][1]) 
+        {
+            loader.ec_point_assert_eq("constraint ec point", lhs_point, rhs_point);
+        } else {
+            panic!("Expected EC points in the transcript objects");
+        }
+
+        if let (TranscriptObject::EcPoint(lhs_point), TranscriptObject::EcPoint(rhs_point)) = 
+            (&proof_transcripts[2][0], &proof_transcripts[3][2]) 
         {
             loader.ec_point_assert_eq("constraint ec point", lhs_point, rhs_point);
         } else {
